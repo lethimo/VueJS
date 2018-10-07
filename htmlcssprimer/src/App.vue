20cfccc92f8e96c9ea2c9271c326ab6f5d2c2bfd
lethimo <lethimo14021997@gmail.com>
2018-10-07 15:35:34 +0700

    commit 1

--- htmlcssprimer/src/App.vue
+++ htmlcssprimer/src/App.vue
@@ -1,13 +1,31 @@
+<!-- Hello word by VueJS
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+-->
 
+<template>
+  <div>
+    <h4 class="bg-primary text-while text-center p-2">Adam's To Do list</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col" >{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- helloworld script
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
@@ -15,7 +33,23 @@ export default {
   }
 }
 </script>
+-->
+<script>
+export default{
+  data:function(){
+    return{
+      tasks:[{action:"Buy flowers", done:false},
+      {action:"Buy flowers", done:false},
+      {action:"Buy flowers", done:false},
+      {action:"Buy flowers", done:false},
+      {action:"Buy flowers", done:false},
 
+      ]
+    }
+  }
+}
+
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
