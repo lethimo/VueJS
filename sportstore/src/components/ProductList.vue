de557e654979fb06a95d5bedb78ec026f7897480
lethimo <lethimo14021997@gmail.com>
2018-10-13 22:14:04 +0700

    Commit 4: Phan trang

--- sportstore/src/components/ProductList.vue
+++ sportstore/src/components/ProductList.vue
@@ -4,19 +4,29 @@
             <h4>
                 {{p.name}} <!--Ten san pham-->
                 <span class="badge badge-pill badge-primary float-right">
-                    {{p.price}}
+                    {{p.price | currency}}
                     </span>
             </h4>
             <div class="card-text bg-white p-1">{{p.description}}</div>
         </div>
+        <page-controls />
     </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
+//import {mapState} from "vuex";
+import {mapGetters} from "vuex";
+import PageControls from "./PageControls";
 export default{
+    components:{ PageControls},
     computed:{
-        ...mapState(["products"])
+        //...mapState(["products"])
+        ...mapGetters({products:"processedProducts"})
+    },
+    filters:{
+        currency(value){
+            return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(value);
+        }
     }
 }
 </script>
