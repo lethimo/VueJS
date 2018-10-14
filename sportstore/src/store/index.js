dcf860653a49898296d64c6ecc6e6d7dfd3189b0
lethimo <lethimo14021997@gmail.com>
2018-10-14 14:36:05 +0700

    Commit 7: Category control

--- sportstore/src/store/index.js
+++ sportstore/src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 const testData = [];
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 18; i++) {
     testData.push({
         id: i,
         name: `Product#${i}`,
@@ -19,15 +19,25 @@ export default new Vuex.Store({
         products: testData,
         productsTotal: testData.length,
         currentPage: 1,
-        pageSize: 4
-            //phan trang
+        pageSize: 5, //size phan trang
+        currentCategory: "All"
     },
     getters: {
-        processedProducts: state => {
+        // processedProducts: state => {
+        //     let index = (state.currentPage - 1) * state.pageSize;
+        //     return state.products.slice(index, index + state.pageSize);
+        // },
+        // pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+        productsFilteredByCategory: state => state.products.filter(p => state.currentCategory == "All" || p.category == state.currentCategory),
+        processedProducts: (state, getters) => {
             let index = (state.currentPage - 1) * state.pageSize;
-            return state.products.slice(index, index + state.pageSize);
+            return getters.productsFilteredByCategory.slice(index, index + state.pageSize);
         },
-        pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+        pageCount: (state, getters) =>
+            Math.ceil(getters.productsFilteredByCategory.length / state.pageSize),
+        categories: state => ["All",
+            ...new Set(state.products.map(p => p.category).sort())
+        ]
     },
     mutations: {
         setCurrentPage(state, page) {
@@ -35,6 +45,10 @@ export default new Vuex.Store({
         },
         setPageSize(state, size) {
             state.pageSize = size;
+            state.currentPage = 1;
+        },
+        setCurrentCategory(state, category) {
+            state.currentCategory = category;
             state.currentPage = 1;
         }
     }
