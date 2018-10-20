ea5df28baa79b29731146d398cd5955ec88dda9a
lethimo <lethimo14021997@gmail.com>
2018-10-20 14:05:12 +0700

    Commit 9: CartSummary

---
+++ sportstore/src/components/CartSummary.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="float-right">
+        <small>
+            Your Cart:
+            <span v-if="itemCount > 0">{{itemCount}} items {{totalPrice|currency}}</span>
+            <span v-else>(empty)</span>
+        </small>
+        <router-link to="/cart" class="btn btn-sm bg-dark text-white" v-bind:disable="itemCount==0">
+            <i class="fa fa-shopping-cart"> </i> 
+        </router-link>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+   computed:{
+       ...mapGetters({
+           itemCount:"cart/itemCount",
+           totalPrice:"cart/totalPrice"
+       })
+   } 
+}
+</script>
+
--- sportstore/src/components/Store.vue
+++ sportstore/src/components/Store.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col bg-dark text-white">
                 <a class="navbar-brand">SPORTS STORE</a>
+                <cart-summary />
             </div>
         </div>
         <div class="row">
@@ -17,12 +18,13 @@
     </div>
 </template>
 <script>
-import ProductList from "./ProductList";
-import CategoryControls from './CategoryControls';
+import ProductList from "./ProductList";  //danh sach san pham
+import CategoryControls from './CategoryControls';   //phan trang
+import CartSummary from './CartSummary';   //icon gio hang
 
 export default{
     components:{
-        ProductList,CategoryControls
+        ProductList,CategoryControls,CartSummary
     }
 }
 </script>
