b7e1d25172ed6f580a8bd16aff27cded42c3be8e
lethimo <lethimo14021997@gmail.com>
2018-10-24 22:57:56 +0700

    Commit 10: Checkout

---
+++ sportstore/src/components/OrderThanks.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="m-2 text-center">
+        <h2>Thanks</h2>
+        <p> Thanks for place your order, which is #{{orderId}}.</p>
+        <p>We will ship your good as soon as possible</p>
+        <router-link to="/" class="btn btn-primary">Return to store</router-link>
+    </div>
+</template>
+<script>
+export default {
+    computed:{
+        orderId(){
+            return this.$route.params.id;
+        }
+    }
+}
+</script>
--- sportstore/src/router/index.js
+++ sportstore/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 
 import Store from "../components/Store";
 import ShoppingCart from "../components/ShoppingCart";
+import Checkout from "../components/Checkout";
+import OrderThanks from "../components/OrderThanks";
 Vue.use(VueRouter);
 
 export default new VueRouter({
@@ -10,6 +12,8 @@ export default new VueRouter({
     routes: [
         { path: "/", component: Store },
         { path: "/cart", component: ShoppingCart },
+        { path: "/checkout", component: Checkout },
+        { path: "/thanks/:id", component: OrderThanks },
         { path: "*", redirect: "/" }
     ]
 
